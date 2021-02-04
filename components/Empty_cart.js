dc81a425bcabe78f30aaab419decc982d907e027
Patryk <patrykkaczmarek7@gmail.com>
2021-02-04 15:06:39 +0100

    styling

--- components/Empty_cart.js
+++ components/Empty_cart.js
@@ -1,16 +1,34 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 const Empty_cart = () => {
   return(
-      <div>
-          <Head>
-            <title>Zacznij zakupy</title>
+      <div className="emptycart container-fluid">
+        <Head>
+          <title>Zacznij zakupy</title>
         </Head>
-        <img 
-          className="img-responsive w-100" 
-          src="/empty_cart1.jpg" 
-          style={{height: "100vh"}}
-        />
+        <div className="row">
+          {/* column */}
+          <div className="col-md w-100" style={{height: "100vh"}}>
+            <div className="box">
+            <div className="container text-center" style={{color: "#52340A"}}>
+              <div className="card border-0 shadow my-5 p-md-4">
+                <div className="card-body p-5">
+                  <h1 className="text-center">Koszyk jest pusty</h1>
+                  <hr/>
+                  <Link href="/products">
+                    <button
+                      className="woodbtn"
+                      >
+                        <a className="woodbtn">produkty</a>
+                    </button>
+                </Link>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
       </div>
   )
 }
